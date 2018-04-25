6c720423eb3b3ffd846df018f1d29fc853f7410e
Dragos Stefanescu <ste.dragos02@gmail.com>
2018-04-25 23:41:07 +0300

    adding files in modules

---
+++ model/src/main/java/domain/Centru.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Centru {
+    Locatie location;
+
+    public Locatie getLocation() {
+        return location;
+    }
+
+    public void setLocation(Locatie location) {
+        this.location = location;
+    }
+}

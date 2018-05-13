9562d6751a4015df56da0c443003f1407a8e117e
ioanasolovan <36422376+ioanasolovan@users.noreply.github.com>
2018-05-13 18:16:09 +0300

    Add files via upload

--- source/model/src/main/java/domain/Doctor.java
+++ source/model/src/main/java/domain/Doctor.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 
 public class Doctor implements Serializable
 {
+
     private String username;
     private String password;
-    private Integer id;
+
+    private String nume;
+    private String prenume;
+    private String cnp;
 
     public Doctor()
     {
@@ -18,15 +22,16 @@ public class Doctor implements Serializable
         this.password = password;
     }
 
-    @Override
-    public String toString()
-    {
-        return "Doctor{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", id=" + id +
-                '}';
+    public Doctor(String username, String password, String prenume,String nume, String cnp) {
+        this.username = username;
+        this.password = password;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.cnp = cnp;
     }
+
+
+
 
     public String getUsername() {
         return username;
@@ -44,16 +49,39 @@ public class Doctor implements Serializable
         this.password = password;
     }
 
-    public Integer getId() {
-        return id;
+
+    public String getNume() {
+        return nume;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setNume(String nume) {
+        this.nume = nume;
     }
 
-    public Doctor(Integer id)
-    {
-        this.id = id;
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public String getCnp() {
+        return cnp;
+    }
+
+    public void setCnp(String cnp) {
+        this.cnp = cnp;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nume='" + nume + '\'' +
+                ", prenume='" + prenume + '\'' +
+                ", cnp='" + cnp + '\'' +
+                '}';
     }
 }
